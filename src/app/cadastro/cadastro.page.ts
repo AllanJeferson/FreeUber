2e7a2c5529a6c4a1668b9537d68eb2941c2b32e6
RaphaelFilipeG <raphaelfilipeg@gmail.com>
2019-12-28 08:35:47 -0300

    Acertos no Login e Cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,11 +1,24 @@
-import { NavController, ToastController } from '@ionic/angular';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  NavController,
+  ToastController
+} from '@ionic/angular';
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {
+  NgForm
+} from '@angular/forms';
+import {
+  AngularFireAuth
+} from '@angular/fire/auth';
 
 interface User {
-  email?: string;
-  password?: string;
+  email ? : string;
+  password ? : string;
+  username ? : string;
+  phone ? : string;
 }
 
 @Component({
@@ -15,29 +28,28 @@ interface User {
 })
 
 export class CadastroPage implements OnInit {
-  @ViewChild('form', {
-      static: true
-    }) form: NgForm;
-    user: User;
+
+  user: User = {
+    email: '',
+    password: '',
+    username: '',
+    phone: '',
+  };
 
   constructor(
-    public afAuth: AngularFireAuth,
-  ) {   }
+    public afAuth: AngularFireAuth, public Nav: NavController
+  ) {}
   async CriarConta() {
-   const user = await this.afAuth.auth.createUserWithEmailAndPassword(
-     this.user.email,
-     this.user.password,
-   );
-
-
+    const phone = this.user.phone;
+    const username = this.user.username;
+    const email = this.user.email;
+    const password = this.user.password;
+    await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    const user2 = await this.afAuth.auth.currentUser;
+    user2.updateProfile({
+      displayName: username,
+    });
+    this.Nav.navigateForward('/');
   }
-
-  ngOnInit() {
-   const user = {
-     email: '',
-     password: '',
-   };
-
-  }
-
+  ngOnInit() {}
 }
