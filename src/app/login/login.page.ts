da671acbfa84fdd3be6fbb1034ee3ca728216685
RaphaelFilipeG <raphaelfilipeg@gmail.com>
2020-01-07 05:28:35 -0300

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -27,11 +27,19 @@ export class LoginPage implements OnInit {
       this.user.password,
     );
     console.log(user);
+    if (user.user.emailVerified === false) {this.Nav.navigateForward('/comfirmação'); }
+     else if (user.user.emailVerified === true) {
+      localStorage.setItem('email', user.user.email);
+      localStorage.setItem('nome', user.user.displayName);
+      this.Nav.navigateForward('/home');
+    } else {
+      alert('Erro de Validação, Contate ao Suporte.');
+    }
   }
 
-  async logout() {
-    await this.afAuth.auth.signOut();
-  }
+  //async logout() {
+    //await this.afAuth.auth.signOut();
+  //}
 
   Cadastro() {
     this.Nav.navigateForward('/cadastro');
