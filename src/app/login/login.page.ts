d2fb4ba7a567c10f3641634583af173672f6edcf
RaphaelFilipeG <raphaelfilipeg@gmail.com>
2020-01-07 06:19:58 -0300

    melhoras no login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,7 +5,9 @@ import { NavController } from '@ionic/angular';
 interface User {
  email: string;
  password: string;
+ loginError: string;
 }
+
 
 @Component({
   selector: 'app-login',
@@ -15,37 +17,38 @@ interface User {
 export class LoginPage implements OnInit {
 
   user: User = {
-    email: 'test@test.com.br',
-    password: 'test',
+    email: '',
+    password: '',
+    loginError: ''
   };
 
   constructor( public afAuth: AngularFireAuth, public Nav: NavController) { }
+ 
 
   async login() {
     const user = await this.afAuth.auth.signInWithEmailAndPassword(
       this.user.email,
       this.user.password,
+    ).then(
+      () => this.Nav.navigateForward('home'),
+      error => this.user.loginError = "E-mail ou Senha Invalidos"
     );
-    console.log(user);
-    if (user.user.emailVerified === false) {this.Nav.navigateForward('/comfirmação'); }
-     else if (user.user.emailVerified === true) {
-      localStorage.setItem('email', user.user.email);
-      localStorage.setItem('nome', user.user.displayName);
-      this.Nav.navigateForward('/home');
-    } else {
-      alert('Erro de Validação, Contate ao Suporte.');
-    }
+// bem vindo {{afAuth.auth.currentUser.displayName}} para mostrar o nome
   }
 
-  //async logout() {
-    //await this.afAuth.auth.signOut();
-  //}
+  // async logout() {
+    // await this.afAuth.auth.signOut();
+  // }
 
   Cadastro() {
     this.Nav.navigateForward('/cadastro');
   }
 
   ngOnInit() {
+    if (this.afAuth.auth.currentUser) {
+      this.Nav.navigateForward('/home');
+    } else {
+    }
   }
 }
 
